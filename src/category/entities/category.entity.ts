9786591253bcb7234b746ef15ce171d42f4a9124
Adem <ademduran6888@gmail.com>
2024-06-27 10:47:25 +0200

    feat(category): making the category crud

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,10 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('category')
+export class CategoryEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
